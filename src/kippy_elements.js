ac2afb46646e0f7f5704ea950044a7642a11a8fd
p4r17y817 <christian.dunn@warwick.ac.uk>
2020-03-28 22:04:24 +0700

    Squashed bugs

--- src/kippy_elements.js
+++ src/kippy_elements.js
@@ -2,36 +2,38 @@ const { BrowserWindow } = require("electron").remote;
 
 class WindowFrame extends Kippy {
     constructor() {
-        super(null, "window-frame");
-
+        super(null, "div", "window-frame");
+    }
+    
+    initialise() {
         this.state = {
             authenticated: true
         }
 
-        this.window = BrowserWindow.getFocusedWindow();
+        this.title_bar = new TitleBar(this);
+    }
+}
+
+class TitleBar extends Kippy {
+    constructor(parent) {
+        super(parent, "div", "title-bar");
     }
 
-    render_content(container) {
-        const title_bar = document.createElement("div");
-        title_bar.className = "title-bar";
+    initialise() {
 
-        if (this.state.authenticated) {
-            const settings = document.createElement("i");
-            settings.className = "fas fa-cog settings";
-            title_bar.appendChild(settings);
+        this.window = BrowserWindow.getFocusedWindow();
 
-            const srql_search = document.createElement("input");
-            srql_search.className = "srql-search";
-            title_bar.appendChild(srql_search);
-        }
+        this.settings = document.createElement("i");
+        this.settings.className = "fas fa-cog settings";
+        this.container.appendChild(this.settings);
 
-        const minimize = document.createElement("button");
-        minimize.innerHTML = `<i class="fas fa-minus"></i>`;
-        minimize.addEventListener("click", () => { this.window.minimize() });
+        this.minimize = document.createElement("button");
+        this.minimize.innerHTML = `<i class="fas fa-minus"></i>`;
+        this.minimize.addEventListener("click", () => { this.window.minimize() });
 
-        const restore = document.createElement("button");
-        restore.innerHTML = `<i class="far fa-window-maximize"></i>`;
-        restore.addEventListener("click", () => {
+        this.restore = document.createElement("button");
+        this.restore.innerHTML = `<i class="far fa-window-maximize"></i>`;
+        this.restore.addEventListener("click", () => {
             if (this.window.isMaximized()) {
                 this.window.unmaximize();
             } else {
@@ -39,15 +41,56 @@ class WindowFrame extends Kippy {
             }
         });
 
-        const close = document.createElement("button");
-        close.className = "close";
-        close.innerHTML = `<i class="fas fa-times"></i>`;
-        close.addEventListener("click", () => { this.window.close() });
+        this.close = document.createElement("button");
+        this.close.className = "close";
+        this.close.innerHTML = `<i class="fas fa-times"></i>`;
+        this.close.addEventListener("click", () => { this.window.close() });
 
-        title_bar.appendChild(close);
-        title_bar.appendChild(restore);
-        title_bar.appendChild(minimize);
+        this.container.appendChild(this.close);
+        this.container.appendChild(this.restore);
+        this.container.appendChild(this.minimize);
 
-        container.appendChild(title_bar);
+        this.srql_search = new SrqlSearch(this);
+    }
+
+    update() {
+        this.settings.style.display = this.parent.state.authenticated ? null : "none";
+        this.srql_search.container.style.display = this.parent.state.authenticated ? null : "none";
+    }
+}
+
+class SrqlSearch extends Kippy {
+    constructor(parent) {
+        super(parent, "input", "srql-search");
+    }
+
+    initialise() {
+        this.state = {
+            text: "start"
+        };
+
+        this.container.addEventListener("input", () => {
+            this.state.text = this.container.value;
+        });
+
+        this.search_suggestion = new SearchSuggestion(this);
+    }
+}
+
+
+class SearchSuggestion extends Kippy {
+    constructor(parent) {
+        super(parent, "div", "search-suggestion");
+    }
+    
+    initialise() {
+        this.state = {
+            suggestions: []
+        };
+    }
+
+    update() {
+        this.container.textContent = this.parent.state.text;
+        console.log(this.parent.state.text);
     }
 }
